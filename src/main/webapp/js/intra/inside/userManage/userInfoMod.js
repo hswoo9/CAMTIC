e81b1f18c90c4beeb207e8bbf5c9e0264d265479
jsy <liveokde1004@dev-jitsu.com>
2023-07-09 20:42:00 +0900

    캠인사이드 - 인사관리 - 인사정보변경신청 완료

--- src/main/webapp/js/intra/inside/userManage/userInfoMod.js
+++ src/main/webapp/js/intra/inside/userManage/userInfoMod.js
@@ -44,45 +44,6 @@ var userInfoMod = {
             ],
             index: 0
         });
-
-        $("#dept").kendoDropDownList({
-            dataTextField: "text",
-            dataValueField: "value",
-            dataSource: [
-				{ text: "전체", value: "" },
-                {text: "미래전략기획본부", value: "미래전략기획본부"},
-                {text: "R&BD사업본부", value: "R&BD사업본부"},
-                {text: "기업성장지원본부", value: "기업성장지원본부"},
-                {text: "우주항공사업부", value: "우주항공사업부"},
-                {text: "드론사업부", value: "드론사업부"},
-                {text: "스마트제조사업부", value: "스마트제조사업부"},
-                {text: "경영지원실", value: "경영지원실"}
-            ],
-            index: 0
-        });
-
-        $("#team").kendoDropDownList({
-            dataTextField: "text",
-            dataValueField: "value",
-            dataSource: [
-				{ text: "전체", value: "" },
-                {text: "제조혁신팀", value: "제조혁신팀"},
-                {text: "신기술융합팀", value: "신기술융합팀"},
-                {text: "우주개발팀", value: "우주개발팀"},
-                {text: "항공개발팀", value: "항공개발팀"},
-                {text: "사업지원팀", value: "사업지원팀"},
-                {text: "인재개발팀", value: "인재개발팀"},
-                {text: "일자리창업팀", value: "일자리창업팀"},
-                {text: "복합소재뿌리기술센터", value: "복합소재뿌리기술센터"},
-                {text: "지역산업육성팀", value: "지역산업육성팀"},
-                {text: "경영지원팀", value: "경영지원팀"},
-                {text: "미래전략기획팀", value: "미래전략기획팀"},
-                {text: "J-밸리혁신팀", value: "J-밸리혁신팀"},
-                {text: "전북 조선업 도약센터", value: "전북 조선업 도약센터"},
-                {text: "익산고용안정일자리센터", value: "익산고용안정일자리센터"}
-            ],
-            index: 0
-        });
 		
         $("#status").kendoDropDownList({
             dataTextField: "text",
@@ -95,6 +56,8 @@ var userInfoMod = {
             ],
             index: 0
         });
+
+        fn_deptSetting();
     },
 
     mainGrid : function(url, params) {
@@ -243,7 +206,7 @@ var userInfoMod = {
             console.log(selectedItem.key);
             console.log(selectedItem.type);
             console.log(selectedItem.ID);
-            if(selectedItem.type == 'dj_emp_educational'){
+            /*if(selectedItem.type == 'dj_emp_educational'){
 	            userInfoMod.userPersonnelRecordPop("degree");				
 			}else if(selectedItem.type == 'dj_emp_family_info'){
 				userInfoMod.userPersonnelRecordPop("family");
@@ -261,7 +224,7 @@ var userInfoMod = {
 				userInfoMod.userPersonnelRecordPop("job");
 			}else if(selectedItem.type == 'dj_emp_career'){
 				userInfoMod.userPersonnelRecordPop("career");
-			}
+			}*/
         });
     },
     fn_approvalTest : function(e) {
