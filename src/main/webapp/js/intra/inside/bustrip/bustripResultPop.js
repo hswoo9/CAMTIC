91996aa1aa921d79471d06628db3fe1f20fa1717
Hanker <anhan0804@dev-jitsu.com>
2023-07-15 11:59:08 +0900

    부서 , 팀 insert 조건 변경

--- src/main/webapp/js/intra/inside/bustrip/bustripResultPop.js
+++ src/main/webapp/js/intra/inside/bustrip/bustripResultPop.js
@@ -16,6 +16,35 @@ var bustripResultPop = {
         customKendo.fn_dropDownList("realDriver", ds.list, "EMP_NAME", "EMP_SEQ");
 
         customKendo.fn_textBox(["result"]);
+    },
+
+    fn_save : function(e){
+        if(!confirm("출장 결과보고를 저장하시겠습니까?")){
+            return;
+        }
+
+        if(realDriver == null || realDriver == ""){
+            alert("운행자를 선택해주세요.");
+            return;
+        } else if (result == null || result == ""){
+            alert("출장결과를 입력해주세요.");
+            return;
+        } else if (moveDst == null || moveDst == ""){
+            alert("운행거리를 입력해주세요.");
+            return;
+        }
+
+        var data = {
+            hrBizReqId : e,
+            driverEmpSeq : $("#realDriver").val(),
+            result : $("#result").val(),
+            moveDst : $("#moveDst").val()
+        }
+
+        var rs = customKendo.fn_customAjax("/bustrip/saveBustripResult", data);
+
+
+
     }
 }
 
