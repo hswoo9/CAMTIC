07a6a09251962ca0e482b0c6ced2da70848ca13c
김지혜 <rlawl0423@dev-jitsu.com>
2023-09-15 19:14:15 +0900

    캠인사이드 > 인사평가 > 직원 면담 카드 - 진행 중

---
+++ src/main/webapp/js/intra/inside/userManage/contentWritePop.js
@@ -0,0 +1,11 @@
+var contentWritePop = {
+    init: function (){
+        contentWritePop.dataSet();
+    },
+
+    dataSet: function(){
+        customKendo.fn_textBox(["content1", "content2", "content3", "content4", "content5"]);
+    },
+
+
+}
