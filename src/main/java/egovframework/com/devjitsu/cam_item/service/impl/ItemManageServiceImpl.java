ac68107cb11f792ecc0a7dbf3ef03f667087ddb0
deer <deer@dev-jitsu.com>
2023-10-19 10:24:35 +0900

    1. BOM - 조회 페이지 오류 수정

--- src/main/java/egovframework/com/devjitsu/cam_item/service/impl/ItemManageServiceImpl.java
+++ src/main/java/egovframework/com/devjitsu/cam_item/service/impl/ItemManageServiceImpl.java
@@ -238,7 +238,7 @@ public class ItemManageServiceImpl implements ItemManageService {
         List<Map<String, Object>> result = new ArrayList<Map<String, Object>>();
 
         bom.put("expanded", true);
-        bom.put("selected", "N");
+        bom.put("base", "Y");
 
         for (Map<String, Object> map : bomDetailList) {
             map.put("expanded", true);
