bd1f275d681e6aaa0e67e77f20aa5e6eee42fb97
esther <estherk773@naver.com>
2023-10-31 16:42:20 +0900

    jsp 수정

--- src/main/webapp/js/intra/inside/subHoliday/subHolidayStat.js
+++ src/main/webapp/js/intra/inside/subHoliday/subHolidayStat.js
@@ -51,11 +51,11 @@ var subHolidayStat = {
             dataValueField: "value",
             dataSource: [
                 { text: "전체", value: ""},
-                { text: "성명", value: "" },
-                { text: "직급", value: "" },
-                { text: "등급", value: "" },
-                { text: "직책", value: "" },
-                { text: "메일주소", value: "" }
+                { text: "성명", value: "1" },
+                { text: "직급", value: "2" },
+                { text: "등급", value: "3" },
+                { text: "직책", value: "4" },
+                { text: "메일주소", value: "5" }
             ],
             index: 0
         });
