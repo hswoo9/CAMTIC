b8d7d05a8f963cbf29fdbc0b94dc58e14a46f738
Hanker <anhan0804@dev-jitsu.com>
2023-11-16 14:26:57 +0900

    여입결의서 목록 조회

--- src/main/webapp/js/intra/cam_mng/payApp/entryList.js
+++ src/main/webapp/js/intra/cam_mng/payApp/entryList.js
@@ -76,6 +76,14 @@ var entryList = {
                     title: "문서번호",
                     width: 120,
                 }, {
+                    title: "적요",
+                    field: "EXNP_BRIEFS",
+                    width: 280,
+                    template: function(e){
+                        console.log(e);
+                        return '<div style="cursor: pointer; font-weight: bold" onclick="exnpList.fn_reqRegPopup('+e.EXNP_SN+', \''+e.PAY_APP_SN+'\')">'+e.EXNP_BRIEFS+'</div>';
+                    }
+                }, {
                     title: "프로젝트 명",
                     field: "PJT_NM",
                     width: 200,
@@ -88,7 +96,7 @@ var entryList = {
                     field: "",
                     width: 120,
                     template: function (e){
-
+                        return "-";
                     }
                 }, {
                     title: "신청일",
