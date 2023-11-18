24c827ee65f554e08868d773c3303ba4d7fa7632
Hanker <anhan0804@dev-jitsu.com>
2023-11-18 20:44:50 +0900

    직원별 참여율 조회 오류 수정

--- src/main/webapp/js/intra/cam_mng/partRate/partRateCommon.js
+++ src/main/webapp/js/intra/cam_mng/partRate/partRateCommon.js
@@ -32,7 +32,7 @@ function fn_monBasicSalary(e){
     /** 산재보험 = (기본급 + 상여금) / 산재보험요율(%)*/
     var accidentInsurance = Math.floor(Math.floor(cnt * (e.ACCIDENT_INSURANCE / 100))/10) * 10;
 
-    var sum = cnt + nationalPension + healthInsurance + longCareInsurance + employInsurance + accidentInsurance + (Math.floor((cnt/12)/10) * 10);
+    var sum = Number(cnt) + Number(nationalPension) + Number(healthInsurance) + Number(longCareInsurance) + Number(employInsurance) + Number(accidentInsurance) + (Math.floor((cnt/12)/10) * 10);
 
     return (Math.floor(sum/10) * 10).toString().toMoney();
 }
