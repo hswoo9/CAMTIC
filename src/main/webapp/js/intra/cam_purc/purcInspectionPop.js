26537ffbd9d4ae9143d7eb6b7348aee7bec2dc68
Hanker <anhan0804@dev-jitsu.com>
2023-11-25 18:16:50 +0900

    워터마크 완료

--- src/main/webapp/js/intra/cam_purc/purcInspectionPop.js
+++ src/main/webapp/js/intra/cam_purc/purcInspectionPop.js
@@ -423,12 +423,56 @@ var pri = {
                 html += '   <td>';
                 html += '   </td>';
                 html += '   <td>';
-                html += '       <input type="button" value="삭제" class="k-button k-rounded k-button-solid k-button-solid-error" onclick="fCommon.fnUploadFile(' + i + ')">'
+                html += '       <input type="button" value="삭제" class="k-button k-rounded k-button-solid k-button-solid-error" onclick="pri.fnUploadFile(' + i + ')">'
                 html += '   </td>';
                 html += '</tr>';
             }
 
             $("#fileGrid").append(html);
         }
-    }
+    },
+
+    fnUploadFile : function(e) {
+        let size = 0;
+        const dataTransfer = new DataTransfer();
+        let fileArray = Array.from(fCommon.global.attFiles);
+        fileArray.splice(e, 1);
+        fileArray.forEach(file => {
+            dataTransfer.items.add(file);
+        });
+
+        fCommon.global.attFiles = dataTransfer.files;
+
+        if(fCommon.global.attFiles.length > 0){
+            $("#fileGrid").find(".addFile").remove();
+
+            var html = '';
+            for (var i = 0; i <fCommon.global.attFiles.length; i++) {
+                size = fCommon.bytesToKB(fCommon.global.attFiles[i].size);
+                html += '<tr style="text-align: center;" class="addFile">';
+                html += '   <td>' + fCommon.global.attFiles[i].name.split(".")[0] + '</td>';
+                html += '   <td>' + fCommon.global.attFiles[i].name.split(".")[1] + '</td>';
+                html += '   <td>' + size + '</td>';
+                html += '   <td>';
+                html += '       <input type="button" value="삭제" class="k-button k-rounded k-button-solid k-button-solid-error" onclick="fCommon.fnUploadFile(' + i + ')">';
+                html += '   </td>';
+                html += '</tr>';
+            }
+
+            $("#fileGrid").append(html);
+        }else{
+            $("#fileGrid").find(".addFile").remove();
+
+            if($("#fileGrid").find("tr").length == 0){
+                $("#fileGrid").html('<tr class="defultTr">' +
+                    '	<td colspan="5" style="text-align: center;padding-top: 10px;">선택된 파일이 없습니다.</td>' +
+                    '</tr>');
+            }
+        }
+
+        if(fCommon.global.attFiles.length == 0){
+            fCommon.global.attFiles = new Array();
+        }
+
+    },
 }
