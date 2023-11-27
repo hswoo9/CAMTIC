e7c7ccec348fae382b836b7fbda8e5d53011f4a8
김민주 <ignore4557@gmail.com>
2023-11-27 21:01:04 +0900

    인사통계현황- 입/퇴사현황 팝업 수정 중

--- src/main/webapp/js/intra/inside/userManage/joinLeaveViewPop.js
+++ src/main/webapp/js/intra/inside/userManage/joinLeaveViewPop.js
@@ -68,20 +68,99 @@ var joinLeaveViewPop = {
                     field: "POSITION_NAME",
                     title: "직급",
                 }, {
-                    field: "OFFICE_TEL_NUM",
-                    title: "전화번호"
+                    field: "",
+                    title: "나이",
+                    template : function(e){
+                        if(e.RES_REGIS_NUM != null && e.RES_REGIS_NUM != "") {
+                            var age = joinLeaveViewPop.fn_setCalcAge(e.RES_REGIS_NUM);
+                            return age;
+                        } else {
+                            return "-";
+                        }
+                    }
                 }, {
-                    field: "MOBILE_TEL_NUM",
-                    title: "핸드폰"
+                    field: "",
+                    title: "근속년수",
+                    template :function(e){
+                        if(e.hire != null && e.hire != ""){
+                            return e.hire + "년 " + e.hire_mon + "개월";
+                        } else {
+                            return "-";
+                        }
+
+                    }
                 }, {
                     field: "JOIN_DAY",
-                    title: "입사일"
+                    title: "입사일",
+                    template: function (dataItem) {
+                        var joinDay = new Date(dataItem.JOIN_DAY);
+
+                        var formattedJoinDay = joinDay.getFullYear() + '-' +
+                            ('0' + (joinDay.getMonth() + 1)).slice(-2) + '-' +
+                            ('0' + joinDay.getDate()).slice(-2);
+
+                        return formattedJoinDay;
+                    }
                 }, {
                     field: "RESIGN_DAY",
                     title: "퇴사일"
                 }
             ]
         }).data("kendoGrid");
+    },
+
+    fn_setCalcAge: function(jumin){
+        // 전달받은 주민번호 데이터에 '-' 확인 후 있으면 제거
+        if(jumin.includes('-')){
+            jumin = jumin.replace('-','');
+        }
+
+        let today = new Date();	// 현재 날짜 및 시간
+
+        let juminFront = jumin.substr(0,6); // 주민번호앞자리
+        let juminBackFirstVal = jumin.substr(6,1); //주민번호뒷자리 첫 문자열(2000년도 이전생인지 확인)
+
+        let age = 0;
+        let birthDate = null;
+        let juminYear = null;
+        let juminMonth = jumin.substr(2,2);//10
+        let juminDate = jumin.substr(4,2);//03
+
+        let monthCheck = 0;
+
+        if(juminBackFirstVal == 1 || juminBackFirstVal == 2){
+            // 2000년생 이전일 경우
+            juminYear = "19" + jumin.substr(0,2);//93~~
+
+            // 문법상 Month(월)은 0부터 시작하기 때문에 -1 처리해야 됨.
+            birthDate = new Date(juminYear*1, juminMonth-1, juminDate*1);
+
+            // 현재 연도에서 - 태어난 연도
+            age = today.getFullYear() - birthDate.getFullYear();
+
+            // 현재 월에서 - 태어난 월
+            monthCheck = today.getMonth() - birthDate.getMonth();
+
+            // 생일 월이 현재 월을 지나지 않았을 경우 만 나이기 때문에 -1
+            if(monthCheck < 0 || (monthCheck === 0 && today.getDate() < birthDate.getDate())){
+                age--;
+            }
+        }else{
+            // 2000년생 이후
+            juminYear = "20" + jumin.substr(0,2);//01~~
+
+            birthDate = new Date(juminYear*1, juminMonth-1, juminDate*1);
+
+            age = today.getFullYear() - birthDate.getFullYear();
+
+            monthCheck = today.getMonth() - birthDate.getMonth();
+
+            if(monthCheck < 0 || (monthCheck === 0 && today.getDate() < birthDate.getDate())){
+                age--;
+            }
+        }
+
+        return age;
     }
 
 
