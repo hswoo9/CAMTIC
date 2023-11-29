25d5baeda315b198340287dca1922c555c855e6e
esther <estherk773@naver.com>
2023-11-30 01:43:59 +0900

    년도별 직급 현황 진행중
    - 김에스더 -

--- src/main/java/egovframework/com/devjitsu/inside/userManage/service/Impl/UserManageServiceImpl.java
+++ src/main/java/egovframework/com/devjitsu/inside/userManage/service/Impl/UserManageServiceImpl.java
@@ -1244,5 +1244,29 @@ public class UserManageServiceImpl implements UserManageService {
     }
 
 
+//    년도별 직급 현황
+@Override
+public List<Map<String, Object>> getPositionNameByYear(Map<String, Object> params){
+    if(params.containsKey("arr") && !"".equals(params.get("arr").toString())){
+        String arrText = params.get("arr").toString();
+
+        String[] arr = arrText.split("[|]");
+        for(int i = 0; i < arr.length; i++){
+            String[] arrL = arr[i].split("&");
+
+            String returnTxt = "(DIVISION IN(" + arrL[0] + ")";
+            if(arrL.length > 1){
+                if(!arrL[1].equals("N")){
+                    returnTxt += " AND DIVISION_SUB IN(" + arrL[1] + ")";
+                }
+            }
+            returnTxt += ")";
+
+            arr[i] = returnTxt;
+        }
+        params.put("arr", arr);
+    }
+    return userManageRepository.getPositionNameByYear(params);
+}
 
 }
