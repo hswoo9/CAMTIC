4490b39167b53b5f110025ae24c9f69e233d4988
jsy <liveokde1004@dev-jitsu.com>
2023-12-14 19:17:41 +0900

    구매청구 업체 다를시 총금액이 합산되서 금액이 두배가 됨

--- src/main/webapp/js/intra/inside/bustrip/bustripReqPop.js
+++ src/main/webapp/js/intra/inside/bustrip/bustripReqPop.js
@@ -324,11 +324,11 @@ const bustripReq = {
                             /** 협업이 아닐때 */
                         }else{
                             if(busnClass == "D"){
-                                opener.window.location.href="/project/pop/viewRegProject.do?pjtSn=" + $("#pjtSn").val() + "&tab=11";
+                                opener.window.location.href="/project/pop/viewRegProject.do?pjtSn=" + $("#pjtSn").val() + "&tab=9";
                             }else if(busnClass == "R"){
-                                opener.window.location.href="/projectRnd/pop/regProject.do?pjtSn=" + $("#pjtSn").val() + "&tab=12";
+                                opener.window.location.href="/projectRnd/pop/regProject.do?pjtSn=" + $("#pjtSn").val() + "&tab=10";
                             }else if(busnClass == "S"){
-                                opener.window.location.href="/projectUnRnd/pop/regProject.do?pjtSn=" + $("#pjtSn").val() + "&tab=12";
+                                opener.window.location.href="/projectUnRnd/pop/regProject.do?pjtSn=" + $("#pjtSn").val() + "&tab=10";
                             }else{
                                 opener.window.location.reload();
                             }
