bc5c481655a6595dab58bf177580f572aca94c39
JHJ <koml777@dev-jitsu.com>
2023-12-22 18:45:47 +0900

    참여율 1.현물참여금액 자동 변경 추가 2.캠프로젝트 > 참여율 현황 화면 수정 3.상태 "종료" 추가

--- src/main/webapp/js/intra/cam_mng/partRate/userPartRate.js
+++ src/main/webapp/js/intra/cam_mng/partRate/userPartRate.js
@@ -27,12 +27,20 @@ var userPartRate = {
             }
         });
 
-        userPartRate.fn_setData();
+        var adminYn = $("#adminYn").val();
+
+        if(adminYn != ""){
+            userPartRate.fn_setDataAdmin();
+        }else{
+            userPartRate.fn_setData();
+        }
+
+        //주민번호
+        $("#resRegisNum").text($("#resRegisNum").text() + "******");
     },
 
 
     fn_setData : function (type){
-        var adminYn = $("#adminYn").val();
         var selStartDate = $("#year").data("kendoDropDownList").value() + "-01-01";
         var selEndDate = $("#year").data("kendoDropDownList").value() + "-12-31";
 
@@ -55,8 +63,11 @@ var userPartRate = {
         var date = new Date(projectStartMonth);
         $("#userPartRateHeader").html("");
         var hdHtml = "";
+        hdHtml += '<th scope="row" class="text-center th-color">과제구분</th>';
         hdHtml += '<th scope="row" class="text-center th-color">지원부처</th>';
-        hdHtml += '<th scope="row" class="text-center th-color">사업명</th>';
+        hdHtml += '<th scope="row" class="text-center th-color">과제명</th>';
+        hdHtml += '<th scope="row" class="text-center th-color">참여시작</th>';
+        hdHtml += '<th scope="row" class="text-center th-color">참여종료</th>';
         hdHtml += '<th scope="row" class="text-center th-color">상태</th>';
         hdHtml += '<th scope="row" class="text-center th-color">참여구분</th>';
         hdHtml += '<th scope="row" class="text-center th-color">5공3책</th>';
@@ -110,23 +121,211 @@ var userPartRate = {
                 for (var i = 0; i < rs.length; i++) {
                     var pjtStatus = "진행";
 
-                    var checkArr = ['Y', 'E', 'E1', 'E2', 'R', 'S'];
-                    if(checkArr.indexOf(rs[i].PJT_STEP) > -1){
+                    var checkIngArr = ['Y', 'E', 'E1', 'E2', 'R', 'S'];
+                    if(checkIngArr.indexOf(rs[i].PJT_STEP) > -1){
                         pjtStatus = "예정";
+                    }
+
+                    var checkEndArr = ['E6', 'E7', 'R3', 'S3'];
+                    if(checkEndArr.indexOf(rs[i].PJT_STEP) > -1){
+                        pjtStatus = "종료";
                     }
 
                     var pm = "";
                     if(parameters.empSeq == rs[i].PM_EMP_SEQ){
                         pm = "책임자";
-                        pmCnt++;
                     } else {
                         pm = "참여자";
                     }
 
                     var sbjStat = "";
                     if(rs[i].SBJ_STAT_YN == "Y"){
+                        if(parameters.empSeq == rs[i].PM_EMP_SEQ){
+                            pmCnt++;
+                        }
+
                         sbjStat = "적용";
                         sbjStatCnt++;
+                    }else{
+                        sbjStat = "미적용";
+                    }
+
+                    var subClassText = "";
+                    if(rs[i].subClassText != null){
+                        subClassText = rs[i].subClassText;
+                    }
+
+                    bodyHtml += '<tr style="text-align: center;">';
+                    bodyHtml += '   <td>'+subClassText+'</td>';
+                    bodyHtml += '   <td>'+rs[i].SBJ_DEP_NM+'</td>';
+                    bodyHtml += '   <td>'+rs[i].PJT_NM+'</td>';
+                    bodyHtml += '   <td>'+rs[i].PART_DET_STR_DT+'</td>';
+                    bodyHtml += '   <td>'+rs[i].PART_DET_END_DT+'</td>';
+                    bodyHtml += '   <td>'+pjtStatus+'</td>';
+                    bodyHtml += '   <td>'+pm+'</td>';
+                    bodyHtml += '   <td>'+sbjStat+'</td>';
+
+
+                    var date = new Date(projectStartMonth);
+
+
+                    var userStrDeArr = rs[i].PART_DET_STR_DT.split("-");
+                    var userEndDeArr = rs[i].PART_DET_END_DT.split("-");
+                    var userStartMonth = userStrDeArr[0] + "-" + userStrDeArr[1];
+
+                    var userDate = new Date(userStartMonth);
+
+
+
+                    for(var j = 0 ; j < diffMonth ; j++){
+                        var dt = date.getFullYear() + "-" + (date.getMonth() + 1);
+                        var userDt = userDate.getFullYear() + "-" + (userDate.getMonth() + 1);
+
+                        userChangeSalaryArr[i][j] = 0;
+                        userMonthSalaryArr[i][j] = 0;
+                        userTotRateArr[i][j] = 0;
+                        if(dt == userDt && new Date(dt) <= new Date(userEndDeArr[0] + "-" + userEndDeArr[1])){
+                            if($("#rateFlag").val() == "B"){
+                                bodyHtml += '<td style="text-align: right">'+comma(rs[i].MON_SAL)+'</td>';
+                            } else {
+                                bodyHtml += '<td>'+rs[i].TOT_RATE+'%</td>';
+                            }
+
+                            userDate.setMonth(userDate.getMonth() + 1);
+
+                            userChangeSalaryArr[i][j] = rs[i].CHNG_SAL;
+                            userMonthSalaryArr[i][j] = rs[i].MON_SAL;
+                            userTotRateArr[i][j] = rs[i].TOT_RATE;
+                        } else {
+                            bodyHtml += '<td></td>';
+                        }
+
+                        date.setMonth(date.getMonth() + 1);
+                    }
+
+                    bodyHtml += '</tr>';
+                }
+
+                var userChangeSalary = 0;
+                bodyHtml += '<tr>';
+                bodyHtml += '   <td colspan="8" class="text-center" style="background-color: #8fa1c04a;">참여율 총합</td>';
+                for(var j = 0 ; j< diffMonth; j++){
+                    var userTotRate = 0;
+                    for(var i = 0 ; i < rs.length; i++){
+                        userTotRate += Number(userTotRateArr[i][j]);
+                    }
+                    bodyHtml += '<td style="text-align: right; font-weight: bold">'+userTotRate.toFixed(1)+'%</td>';
+                }
+                bodyHtml += '</tr>';
+
+                bodyHtml += '<tr>';
+                bodyHtml += '   <td colspan="8" class="text-center" style="background-color: #8fa1c04a;"></td>';
+                for(var j = 0 ; j< diffMonth; j++){
+                    bodyHtml += '<td style="text-align: right; font-weight: bold">'+sbjStatCnt+'공 '+ pmCnt + '책</td>';
+                }
+                bodyHtml += '</tr>';
+
+                $("#userPartRateBody").html(bodyHtml);
+            }
+        });
+    },
+
+    fn_setDataAdmin : function (type){
+        var selStartDate = $("#year").data("kendoDropDownList").value() + "-01-01";
+        var selEndDate = $("#year").data("kendoDropDownList").value() + "-12-31";
+
+        $("#rateFlag").val(type);
+
+        var strDe = selStartDate.split("-");
+        var endDe = selEndDate.split("-");
+
+        var diffMonth = (endDe[0] - strDe[0]) * 12 + (endDe[1] - strDe[1]) + 1;
+
+        const projectStartMonth = strDe[0] + "-01";
+        var date = new Date(projectStartMonth);
+        $("#userPartRateHeader").html("");
+        var hdHtml = "";
+        hdHtml += '<th scope="row" class="text-center th-color">지원부처</th>';
+        hdHtml += '<th scope="row" class="text-center th-color">사업명</th>';
+        hdHtml += '<th scope="row" class="text-center th-color">상태</th>';
+        hdHtml += '<th scope="row" class="text-center th-color">참여구분</th>';
+        hdHtml += '<th scope="row" class="text-center th-color">5공3책</th>';
+
+
+        diffMonth = 12;
+
+        for(var i = 0 ; i < diffMonth ; i++){
+            var dtMonth = date.getMonth() + 1;
+            if(dtMonth.toString().length == 1){
+                dtMonth = "0" + dtMonth;
+            }
+            hdHtml += '<th scope="row" class="text-center th-color">'+date.getFullYear() + '-' + dtMonth +'</th>';
+
+            date.setMonth(date.getMonth() + 1);
+
+        }
+
+        $("#userPartRateHeader").html(hdHtml);
+
+        var parameters = {
+            empSeq : $("#userEmpSeq").val(),
+            strDe : selStartDate,
+            diffMon : diffMonth,
+            strMonth : projectStartMonth + "-01",
+        }
+
+        if(parameters.strDe == ""){
+            parameters.strDe = $("#bsStrDt").val();
+        }
+
+        $.ajax({
+            url : "/mng/userPartRateInfo",
+            data : parameters,
+            type : "post",
+            dataType : "json",
+            success : function (rs){
+                var salList = rs.userSalList;
+                var rs = rs.list;
+
+                $("#userPartRateBody").html("");
+                var bodyHtml = "";
+
+
+
+                var userChangeSalaryArr = fn_create2DArray(rs.length, diffMonth);
+                var userMonthSalaryArr = fn_create2DArray(rs.length, diffMonth);
+                var userTotRateArr = fn_create2DArray(rs.length, diffMonth);
+                var pmCnt = 0;
+                var sbjStatCnt = 0;
+                for (var i = 0; i < rs.length; i++) {
+                    var pjtStatus = "진행";
+
+                    var checkIngArr = ['Y', 'E', 'E1', 'E2', 'R', 'S'];
+                    if(checkIngArr.indexOf(rs[i].PJT_STEP) > -1){
+                        pjtStatus = "예정";
+                    }
+
+                    var checkEndArr = ['E6', 'E7', 'R3', 'S3'];
+                    if(checkEndArr.indexOf(rs[i].PJT_STEP) > -1){
+                        pjtStatus = "종료";
+                    }
+
+                    var pm = "";
+                    if(parameters.empSeq == rs[i].PM_EMP_SEQ){
+                        pm = "책임자";
+                    } else {
+                        pm = "참여자";
+                    }
+
+                    var sbjStat = "";
+                    if(rs[i].SBJ_STAT_YN == "Y"){
+                        if(parameters.empSeq == rs[i].PM_EMP_SEQ){
+                            pmCnt++;
+                        }
+                        sbjStat = "적용";
+                        sbjStatCnt++;
+                    }else{
+                        sbjStat = "미적용";
                     }
 
                     bodyHtml += '<tr style="text-align: center;">';
@@ -179,30 +378,28 @@ var userPartRate = {
 
                 var userChangeSalary = 0;
 
-                if(adminYn != "") {
-                    bodyHtml += "<tr>";
-                    bodyHtml += "   <td colspan='5' class='text-center' style='background-color: #8fa1c04a;'>월지급액</td>";
+                bodyHtml += "<tr>";
+                bodyHtml += "   <td colspan='5' class='text-center' style='background-color: #8fa1c04a;'>월지급액</td>";
 
-                    for (var j = 0; j < diffMonth; j++) {
-                        var userMonthSalary = 0;
-                        for (var i = 0; i < rs.length; i++) {
-                            userMonthSalary += userMonthSalaryArr[i][j];
-                        }
+                for (var j = 0; j < diffMonth; j++) {
+                    var userMonthSalary = 0;
+                    for (var i = 0; i < rs.length; i++) {
+                        userMonthSalary += userMonthSalaryArr[i][j];
+                    }
 
-                        bodyHtml += '<td style="text-align: right; font-weight: bold">' + comma(userMonthSalary) + '</td>';
-                    }
-                    bodyHtml += '</tr>';
-                    bodyHtml += "<tr>";
-                    bodyHtml += "   <td colspan='5' class='text-center' style='background-color: #8fa1c04a;'>기준급여</td>";
-                    for (var j = 0; j < diffMonth; j++) {
-                        if (salList[j] == null) {
-                            bodyHtml += '<td style="text-align: right; font-weight: bold">0</td>';
-                        } else {
-                            bodyHtml += '<td style="text-align: right; font-weight: bold">' + fn_monBasicSalary(salList[j]) + '</td>';
-                        }
-                    }
-                    bodyHtml += '</tr>';
+                    bodyHtml += '<td style="text-align: right; font-weight: bold">' + comma(userMonthSalary) + '</td>';
                 }
+                bodyHtml += '</tr>';
+                bodyHtml += "<tr>";
+                bodyHtml += "   <td colspan='5' class='text-center' style='background-color: #8fa1c04a;'>기준급여</td>";
+                for (var j = 0; j < diffMonth; j++) {
+                    if (salList[j] == null) {
+                        bodyHtml += '<td style="text-align: right; font-weight: bold">0</td>';
+                    } else {
+                        bodyHtml += '<td style="text-align: right; font-weight: bold">' + fn_monBasicSalary(salList[j]) + '</td>';
+                    }
+                }
+                bodyHtml += '</tr>';
                 bodyHtml += '<tr>';
                 bodyHtml += '   <td colspan="5" class="text-center" style="background-color: #8fa1c04a;">사업참여율</td>';
                 for(var j = 0 ; j< diffMonth; j++){
