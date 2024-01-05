f602e10b0d99616918acc1165c0df3de16fddb95
Hanker <anhan0804@dev-jitsu.com>
2024-01-05 10:45:44 +0900

    세금계산서 G20 동기화 추가

--- src/main/webapp/js/intra/cam_mng/etax/etaxList.js
+++ src/main/webapp/js/intra/cam_mng/etax/etaxList.js
@@ -97,7 +97,7 @@ var etaxList = {
                         }
                     }
                 }, {
-                    title: "발급일자",
+                    title: "작성일자",
                     width: 120,
                     template: function (e){
                         return e.ISU_DT.substring(0,4) + "-" + e.ISU_DT.substring(4,6) + "-" + e.ISU_DT.substring(6,8);
