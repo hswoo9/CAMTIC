1596df74500de3132324745e94715b7f563a649e
Hanker <anhan0804@dev-jitsu.com>
2024-01-11 10:13:07 +0900

    지급신청 수정

--- src/main/java/egovframework/com/devjitsu/cam_manager/service/impl/PayAppServiceImpl.java
+++ src/main/java/egovframework/com/devjitsu/cam_manager/service/impl/PayAppServiceImpl.java
@@ -97,10 +97,12 @@ public class PayAppServiceImpl implements PayAppService {
                 fileOrgName = fileOrgName.substring(0, fileOrgName.length() - 1);
                 list.get(i).put("fileExt", fileName[fileName.length - 1]);
                 list.get(i).put("fileOrgName", fileOrgName);
-            }
-            commonRepository.insFileInfo(list);
-        }
 
+                commonRepository.insFileInfoOne(list.get(i));
+                commonRepository.insPayAppFileList(list.get(i));
+            }
+//            commonRepository.insFileInfo(list);
+        }
     }
 
     @Override
