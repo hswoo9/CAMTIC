efb823508d6c107ef8edb67e003295adf1b178e1
jsy <liveokde1004@dev-jitsu.com>
2024-01-27 16:59:14 +0900

    1. 프로젝트 이력 : 캠비즈 링크 연결 완료

---
+++ src/main/java/egovframework/com/devjitsu/g20/repository/PRJRepository.java
@@ -0,0 +1,22 @@
+package egovframework.com.devjitsu.g20.repository;
+
+import egovframework.com.devjitsu.gw.login.repository.AbstractDAO;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class PRJRepository extends AbstractDAO {
+    public List<Map<String, Object>> getHistEngnList(Map<String, Object> params) {
+        return selectListMs("prj.getHistEngnList", params);
+    }
+
+    public List<Map<String, Object>> getHistRndList(Map<String, Object> params) {
+        return selectListMs("prj.getHistEngnList", params);
+    }
+
+    public List<Map<String, Object>> getHistEduList(Map<String, Object> params) {
+        return selectListMs("prj.getHistEngnList", params);
+    }
+}
