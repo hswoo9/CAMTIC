38f97ceeec9f2266fae69e2e5454f14920d5bcf1
jsy <liveokde1004@dev-jitsu.com>
2024-02-19 02:27:43 +0900

    1. 수정사항 (커밋오류로 분할커밋 진행)

--- src/main/webapp/js/intra/inside/bustrip/business/businessExnp.js
+++ src/main/webapp/js/intra/inside/bustrip/business/businessExnp.js
@@ -22,12 +22,16 @@ var busiExnp = {
                 html += '<td style="text-align: center"><button type="button" class="k-button k-button-solid-info" onclick="bustPop.fn_busiPayAppPopup('+e.BF_PAY_APP_SN+', 2)">지급신청</button></td>';
             }
 
-            if(p.EXP_STAT == null){
-                html += '<td style="text-align: center">-</td>';
-            }else if(p.PAY_APP_SN != ""){
-                html += '<td style="text-align: center"><button type="button" class="k-button k-button-solid-base" onclick="bustPop.fn_reqRegPopup('+p.HR_BIZ_REQ_RESULT_ID+')">지급신청</button></td>';
+            if(p != null){
+                if(p.EXP_STAT == null){
+                    html += '<td style="text-align: center">-</td>';
+                }else if(p.PAY_APP_SN != ""){
+                    html += '<td style="text-align: center"><button type="button" class="k-button k-button-solid-base" onclick="bustPop.fn_reqRegPopup('+p.HR_BIZ_REQ_RESULT_ID+')">지급신청</button></td>';
+                }else{
+                    html += '<td style="text-align: center"><button type="button" class="k-button k-button-solid-info" onclick="bustPop.fn_reqRegPopup('+p.PAY_APP_SN+', 2)">지급신청</button></td>';
+                }
             }else{
-                html += '<td style="text-align: center"><button type="button" class="k-button k-button-solid-info" onclick="bustPop.fn_reqRegPopup('+p.PAY_APP_SN+', 2)">지급신청</button></td>';
+                html += '<td style="text-align: center">-</td>';
             }
 
             $("#buttonTr").html(html);
