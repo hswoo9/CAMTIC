6687e5267cdae89d4d37d5550b97a6422c8ac699
jsy <liveokde1004@dev-jitsu.com>
2024-03-11 11:14:12 +0900

    1. 채용 캠틱 온라인 입사지원 팝업 : 다른 공고 지원자 뜨는 오류 수정

--- src/main/webapp/js/intra/inside/recruit/recruitAdminPop.js
+++ src/main/webapp/js/intra/inside/recruit/recruitAdminPop.js
@@ -373,6 +373,7 @@ var recruitAdminPop = {
         if($("#stat").val() == "view"){
             url += "&stat=view";
         }
+        url += "&recruitInfoSn="+$("#recruitInfoSn").val();
         var name = "recruitReqPop";
         var option = "width=1000, height=1200, scrollbars=no, top=100, left=200, resizable=no, toolbars=no, menubar=no"
         var popup = window.open(url, name, option);
