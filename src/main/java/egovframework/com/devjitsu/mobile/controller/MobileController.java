cedf51e5883b147ce4c5b1b3e7cc2f91ceb1bf8a
Hanker <anhan0804@dev-jitsu.com>
2024-03-14 12:02:25 +0900

    모바일 컨트롤러

---
+++ src/main/java/egovframework/com/devjitsu/mobile/controller/MobileController.java
@@ -0,0 +1,75 @@
+package egovframework.com.devjitsu.mobile.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MobileController {
+
+    @RequestMapping("/m/login.do")
+    public String mobileLogin() {
+        return "mobile/login";
+    }
+
+    @RequestMapping("/m/payment.do")
+    public String mobileMain() {
+        return "mobile/payment";
+    }
+
+    @RequestMapping("/m/payment_view.do")
+    public String mobileNotice() {
+        return "mobile/payment_view";
+    }
+
+    @RequestMapping("/m/payment_write.do")
+    public String mobileNotice2() {
+        return "mobile/payment_write";
+    }
+
+    @RequestMapping("/m/organization.do")
+    public String mobileNotice3() {
+        return "mobile/organization";
+    }
+
+    @RequestMapping("/m/organization_view.do")
+    public String mobileNotice4() {
+        return "mobile/organization_view";
+    }
+
+    @RequestMapping("/m/organization_write.do")
+    public String mobileNotice5() {
+        return "mobile/organization_write";
+    }
+
+    @RequestMapping("/m/schedule.do")
+    public String mobileNotice6() {
+        return "mobile/schedule";
+    }
+
+    @RequestMapping("/m/schedule_staff.do")
+    public String mobileNotice7() {
+        return "mobile/schedule_staff";
+    }
+
+    @RequestMapping("/m/board.do")
+    public String mobileNotice8() {
+        return "mobile/board";
+    }
+
+    @RequestMapping("/m/board_view.do")
+    public String mobileNotice9() {
+        return "mobile/board_view";
+    }
+
+    @RequestMapping("/m/board_write.do")
+    public String mobileNotice10() {
+        return "mobile/board_write";
+    }
+
+    @RequestMapping("/m/admin.do")
+    public String mobileNotice11() {
+        return "mobile/admin";
+    }
+
+}
