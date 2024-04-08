afd6e3c020dbf492ec3f861ea5235fb2ee88fc4b
deer <deer@dev-jitsu.com>
2024-04-09 01:18:39 +0900

    1. 직원급여관리 엑셀 다운로드

--- src/main/webapp/js/intra/inside/salaryManage/empSalaryManage.js
+++ src/main/webapp/js/intra/inside/salaryManage/empSalaryManage.js
@@ -329,70 +329,7 @@ var esm = {
                 record = fn_getRowNum(this, 2);
             },
             excelExport : function (e){
-                var sheet = e.workbook.sheets[0];
-                sheet.rows[0].cells.push({
-                    background : "#7a7a7a",
-                    color : "#fff",
-                    firstCell : false,
-                    rowSpan : 2,
-                    value : "국민연금"
-                },{
-                    background : "#7a7a7a",
-                    color : "#fff",
-                    firstCell : false,
-                    rowSpan : 2,
-                    value : "건강보험"
-                },{
-                    background : "#7a7a7a",
-                    color : "#fff",
-                    firstCell : false,
-                    rowSpan : 2,
-                    value : "장기요양 보험"
-                },{
-                    background : "#7a7a7a",
-                    color : "#fff",
-                    firstCell : false,
-                    rowSpan : 2,
-                    value : "고용보험"
-                },{
-                    background : "#7a7a7a",
-                    color : "#fff",
-                    firstCell : false,
-                    rowSpan : 2,
-                    value : "산재보험"
-                },{
-                    background : "#7a7a7a",
-                    color : "#fff",
-                    firstCell : false,
-                    rowSpan : 2,
-                    value : "사대보험 사업자부담분"
-                },{
-                    background : "#7a7a7a",
-                    color : "#fff",
-                    firstCell : false,
-                    rowSpan : 2,
-                    value : "퇴직금 추계액"
-                },{
-                    background : "#7a7a7a",
-                    color : "#fff",
-                    firstCell : false,
-                    rowSpan : 2,
-                    value : "기준급여"
-                });
-                sheet.rows = sheet.rows.filter(h => h.type == "header");
-                $.each($("#mainGrid tbody > tr"), function(i, v){
-                    var data = {
-                        type : "data",
-                        cells : new Array(),
-                    }
-                    $.each($(v).find("td[role='gridcell']:not(:eq(0))"), function(a, b){
-                        var cellsData = {
-                            value : $(b).text()
-                        };
-                        data.cells.push(cellsData);
-                    })
-                    sheet.rows.push(data);
-                })
+                e.workbook.sheets[0] = esm.fn_excelDataSet(e);
             }
         }).data("kendoGrid");
 
@@ -435,9 +372,234 @@ var esm = {
         var option = "width = 500, height = 230, top = 100, left = 400, location = no"
         var popup = window.open(url, name, option);
     },
+
+    fn_excelDataSet : function(e) {
+        var sheet = e.workbook.sheets[0];
+        sheet.rows[0].cells.push({
+            background: "#7a7a7a",
+            color: "#fff",
+            firstCell: false,
+            rowSpan: 2,
+            value: "국민연금"
+        }, {
+            background: "#7a7a7a",
+            color: "#fff",
+            firstCell: false,
+            rowSpan: 2,
+            value: "건강보험"
+        }, {
+            background: "#7a7a7a",
+            color: "#fff",
+            firstCell: false,
+            rowSpan: 2,
+            value: "장기요양 보험"
+        }, {
+            background: "#7a7a7a",
+            color: "#fff",
+            firstCell: false,
+            rowSpan: 2,
+            value: "고용보험"
+        }, {
+            background: "#7a7a7a",
+            color: "#fff",
+            firstCell: false,
+            rowSpan: 2,
+            value: "산재보험"
+        }, {
+            background: "#7a7a7a",
+            color: "#fff",
+            firstCell: false,
+            rowSpan: 2,
+            value: "사대보험 사업자부담분"
+        }, {
+            background: "#7a7a7a",
+            color: "#fff",
+            firstCell: false,
+            rowSpan: 2,
+            value: "퇴직금 추계액"
+        }, {
+            background: "#7a7a7a",
+            color: "#fff",
+            firstCell: false,
+            rowSpan: 2,
+            value: "기준급여"
+        });
+
+        sheet.rows = sheet.rows.filter(h => h.type == "header");
+
+        for (var i = 0; i < e.sender.dataSource._data.length; i++) {
+            var data = {
+                type: "data",
+                cells: new Array(),
+            }
+            for (var j = 0; j < 19; j++) {
+                var cellsData = {};
+                if (j == 0) {
+                    cellsData.value = e.sender.dataSource._data[i].EMP_NAME_KR;
+                } else if (j == 1) {
+                    cellsData.value = e.sender.dataSource._data[i].RES_REGIS_NUM;
+                } else if (j == 2) {
+                    cellsData.value = e.sender.dataSource._data[i].JOIN_DAY;
+                } else if (j == 3) {
+                    cellsData.value = e.sender.dataSource._data[i].DIVISION;
+                } else if (j == 4) {
+                    cellsData.value = e.sender.dataSource._data[i].DEPT_NAME;
+                } else if (j == 5) {
+                    cellsData.value = e.sender.dataSource._data[i].START_DT;
+                } else if (j == 6) {
+                    cellsData.value = e.sender.dataSource._data[i].END_DT;
+                } else if (j == 7) {
+                    cellsData.value = e.sender.dataSource._data[i].BASIC_SALARY;
+                } else if (j == 8) {
+                    cellsData.value = e.sender.dataSource._data[i].FOOD_PAY;
+                } else if (j == 9) {
+                    cellsData.value = e.sender.dataSource._data[i].EXTRA_PAY;
+                } else if (j == 10) {
+                    cellsData.value = e.sender.dataSource._data[i].BONUS;
+                } else if (j == 11) {
+                    /** 국민연금 */
+                    cellsData.value = annuities(e.sender.dataSource._data[i]);
+                } else if (j == 12) {
+                    /** 건강보험 */
+                    cellsData.value = insurance1(e.sender.dataSource._data[i]);
+                } else if (j == 13) {
+                    /** 장기요양 보험 */
+                    cellsData.value = insurance2(e.sender.dataSource._data[i]);
+                } else if (j == 14) {
+                    /** 고용보험 */
+                    cellsData.value = insurance3(e.sender.dataSource._data[i]);
+                } else if (j == 15) {
+                    /** 산재보험 */
+                    cellsData.value = insurance4(e.sender.dataSource._data[i]);
+                } else if (j == 16) {
+                    /** 사대보험 사업자부담분 */
+                    cellsData.value = insuranceComp(e.sender.dataSource._data[i]);
+                } else if (j == 17) {
+                    /** 퇴직금 추계액 */
+                    cellsData.value = retirePay(e.sender.dataSource._data[i]);
+                } else if (j == 18) {
+                    /** 기준급여 */
+                    cellsData.value = baseSalary(e.sender.dataSource._data[i]);
+                }
+                data.cells.push(cellsData);
+                console.log(data.cells);
+            }
+            sheet.rows.push(data);
+            console.log(sheet.rows);
+        }
+
+
+        return sheet;
+    },
 }
 
 
 function userSearch() {
     window.open("/common/deptListPop.do?type=cardMng", "조직도", "width=750, height=650");
+}
+
+function annuities(e){
+    /** 국민연금 = (기본급 + 상여금)/ 국민연금요율(%) */
+    var cnt = Number(e.BASIC_SALARY) + Number(e.EXTRA_PAY) + Number(e.BONUS);
+    var nationalPension = Math.floor(Math.floor(cnt * (e.NATIONAL_PENSION / 100))/10)*10;
+
+    if(nationalPension > Number(e.LIMIT_AMT)){
+        return e.LIMIT_AMT.toString().toMoney()
+    }else{
+        return nationalPension.toString().toMoney()
+    }
+}
+
+function insurance1(e){
+    /** 건강보험 = (기본급 + 상여금) / 건강보험요율(%)*/
+    var cnt = Number(e.BASIC_SALARY) + Number(e.EXTRA_PAY) + Number(e.BONUS);
+    return (Math.floor(Math.floor(cnt * (e.HEALTH_INSURANCE / 100))/10) * 10).toString().toMoney()
+}
+
+function insurance2(e){
+    /** 장기요양보험 = (건강보험합계 / 장기요양보험요율(%))*/
+    var cnt = Number(e.BASIC_SALARY) + Number(e.EXTRA_PAY) + Number(e.BONUS);
+    var healthInsuranceCnt = Math.floor(Math.floor(cnt * (e.HEALTH_INSURANCE / 100))/10) * 10;
+    return (Math.floor(Math.floor(healthInsuranceCnt * (e.LONG_CARE_INSURANCE / 100)) / 10) * 10).toString().toMoney()
+}
+
+function insurance3(e){
+    /** 고용보험 = (기본급 + 상여금) / 고용보험요율(%)*/
+    var cnt = Number(e.BASIC_SALARY) + Number(e.EXTRA_PAY) + Number(e.BONUS);
+    return (Math.floor(Math.floor(cnt * (e.EMPLOY_INSURANCE / 100))/10) * 10).toString().toMoney()
+}
+
+function insurance4(e){
+    /** 산재보험 = (기본급 + 상여금) / 산재보험요율(%)*/
+    var cnt = Number(e.BASIC_SALARY) + Number(e.EXTRA_PAY) + Number(e.BONUS);
+    return (Math.floor(Math.floor(cnt * (e.ACCIDENT_INSURANCE / 100))/10) * 10).toString().toMoney()
+}
+
+function insuranceComp(e){
+    if(e.BUSN_PAY != null && e.BUSN_PAY != "") {
+        return e.BUSN_PAY;
+    }else{
+        /** 기본급 */
+        var cnt = Number(e.BASIC_SALARY) + Number(e.EXTRA_PAY) + Number(e.BONUS);
+
+        /** 국민연금 */
+        var nationalPension = Math.floor(cnt * (e.NATIONAL_PENSION / 100));
+        if(nationalPension > Number(e.LIMIT_AMT)){
+            nationalPension = Number(e.LIMIT_AMT);
+        }
+        /** 건강보험 */
+        var healthInsurance = Math.floor(Math.floor(cnt * (e.HEALTH_INSURANCE / 100))/10) * 10
+        /** 장기요양보험 */
+        var longCareInsurance =  Math.floor(Math.floor(healthInsurance * (e.LONG_CARE_INSURANCE / 100)) / 10) * 10
+        /** 고용보험 */
+        var employInsurance = Math.floor(Math.floor(cnt * (e.EMPLOY_INSURANCE / 100))/10) * 10;
+        /** 산재보험 = (기본급 + 상여금) / 산재보험요율(%)*/
+        var accidentInsurance = Math.floor(Math.floor(cnt * (e.ACCIDENT_INSURANCE / 100))/10) * 10;
+
+        return (nationalPension + healthInsurance + longCareInsurance + employInsurance + accidentInsurance).toString().toMoney();
+    }
+}
+
+function retirePay(e){
+    if(e.RETIRE_PAY != null && e.RETIRE_PAY != ""){
+        return  Number(e.RETIRE_PAY).toString().toMoney();;
+    }else{
+        /** 퇴직금 추계액 = (기본급 + 식대 + 수당 + 상여)/12 */
+        var cnt = Number(e.BASIC_SALARY) + Number(e.FOOD_PAY) + Number(e.EXTRA_PAY) + Number(e.BONUS);
+        return (Math.floor((cnt/12)/10) * 10).toString().toMoney();
+    }
+}
+
+function baseSalary(e){
+    /** 기준급여 = (기본급 + 수당 + 상여 + 사업자부담분 + 퇴직금추계액) */
+    /** 기본급 */
+    var cnt = Number(e.BASIC_SALARY) + Number(e.FOOD_PAY) + Number(e.EXTRA_PAY) + Number(e.BONUS);
+
+    /** 국민연금 */
+    var nationalPension = cnt * (e.NATIONAL_PENSION / 100);
+    if(nationalPension > Number(e.LIMIT_AMT)){
+        nationalPension = Number(e.LIMIT_AMT);
+    }
+    /** 건강보험 */
+    var healthInsurance = Math.floor(Math.floor(cnt * (e.HEALTH_INSURANCE / 100))/10) * 10
+    /** 장기요양보험 */
+    var longCareInsurance =  Math.floor(Math.floor(healthInsurance * (e.LONG_CARE_INSURANCE / 100)) / 10) * 10
+    /** 고용보험 */
+    var employInsurance = Math.floor(Math.floor(cnt * (e.EMPLOY_INSURANCE / 100))/10) * 10;
+    /** 산재보험 = (기본급 + 상여금) / 산재보험요율(%)*/
+    var accidentInsurance = Math.floor(Math.floor(cnt * (e.ACCIDENT_INSURANCE / 100))/10) * 10;
+
+    var sum = cnt + nationalPension + healthInsurance + longCareInsurance + employInsurance + accidentInsurance + (Math.floor((cnt/12)/10) * 10);
+
+    if(e.BUSN_PAY != null && e.BUSN_PAY != ""){
+        sum += Number(e.BUSN_PAY);
+    }else{
+        sum += nationalPension + healthInsurance + longCareInsurance + employInsurance + accidentInsurance;
+    }
+
+    if(e.RETIRE_PAY != null && e.RETIRE_PAY != ""){
+        sum += Number(e.RETIRE_PAY);
+    }
+
+    return (Math.floor(sum/10) * 10).toString().toMoney();
 }
