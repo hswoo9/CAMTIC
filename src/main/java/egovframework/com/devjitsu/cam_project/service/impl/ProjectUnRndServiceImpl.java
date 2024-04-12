cf23f3b0bc5e9e9ee17f48c31ccecb35a8af6fc3
jsy <liveokde1004@dev-jitsu.com>
2024-04-12 14:51:57 +0900

    1. 캠프로젝트 결과보고 첨부파일 설계이미지, 제작이미지에서 납품서로 변경

--- src/main/java/egovframework/com/devjitsu/cam_project/service/impl/ProjectUnRndServiceImpl.java
+++ src/main/java/egovframework/com/devjitsu/cam_project/service/impl/ProjectUnRndServiceImpl.java
@@ -785,13 +785,8 @@ public class ProjectUnRndServiceImpl implements ProjectUnRndService {
 
             /** STEP2. resultData 에서 DSGN_FILE_SN, PROD_FILE_SN 있으면 update */
             if (resultMap != null && !resultMap.isEmpty()) {
-                if(resultMap.containsKey("DSGN_FILE_SN")){
-                    params.put("fileNo", resultMap.get("DSGN_FILE_SN").toString());
-                    projectRepository.setResultFileDocNm(params);
-                }
-
-                if(resultMap.containsKey("PROD_FILE_SN")){
-                    params.put("fileNo", resultMap.get("PROD_FILE_SN").toString());
+                if(resultMap.containsKey("DEV_FILE_SN")){
+                    params.put("fileNo", resultMap.get("DEV_FILE_SN").toString());
                     projectRepository.setResultFileDocNm(params);
                 }
             }
