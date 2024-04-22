1e271e499b25824ac04f72992d33a43ca96b95be
jsy <liveokde1004@dev-jitsu.com>
2024-04-23 01:13:27 +0900

    1. 캠프로젝트 알앤디 버튼 보완

--- src/main/webapp/js/intra/cam_project/rnd/rndDevPlan.js
+++ src/main/webapp/js/intra/cam_project/rnd/rndDevPlan.js
@@ -702,13 +702,13 @@ var rndDP = {
                             buttonHtml = "<button type=\"button\" id=\"devDelBtn\" style=\"float: right; margin-bottom: 5px;\" class=\"k-button k-button-solid-error\" onclick=\"devInfo.fn_delete()\">삭제</button>";
                             buttonHtml += "<button type=\"button\" id=\"devSaveBtn\" style=\"float: right; margin-right: 5px\" class=\"k-button k-button-solid-info\" onclick=\"rndDP.fn_save()\">저장</button>";
                             buttonHtml += "<button type=\"button\" id=\"addVerBtn2\" style=\"float: right; margin-bottom: 5px; margin-right: 5px;\" class=\"k-button k-button-solid-base\" onclick=\"rndDP.fn_addVersion()\">수행계획서 추가</button>";
-                            buttonHtml += "<button type=\"button\" id=\"teamAppBtn\" style=\"float: right; margin-right: 5px\" class=\"k-button k-button-solid-info\" onclick=\"rndDP.fn_teamApp('Y')\">공정 마감</button>";
+                            buttonHtml += "<button type=\"button\" id=\"devTeamAppBtn\" style=\"float: right; margin-right: 5px\" class=\"k-button k-button-solid-info\" onclick=\"rndDP.fn_teamApp('Y')\">공정 마감</button>";
                         }else{
                             buttonHtml = "<button type=\"button\" id=\"devDelBtn\" style=\"float: right; margin-bottom: 5px;\" class=\"k-button k-button-solid-error\" onclick=\"devInfo.fn_delete()\">삭제</button>";
                             buttonHtml += "<button type=\"button\" id=\"devSaveBtn\" style=\"float: right; margin-right: 5px\" class=\"k-button k-button-solid-info\" onclick=\"rndDP.fn_save()\">저장</button>";
                         }
                     }else {
-                        buttonHtml = "<button type=\"button\" id=\"teamAppBtn\" style=\"float: right; margin-bottom: 10px\" class=\"k-button k-button-solid-error\" onclick=\"rndDP.fn_teamApp('N')\">마감취소</button>";
+                        buttonHtml = "<button type=\"button\" id=\"devTeamAppBtn\" style=\"float: right; margin-bottom: 10px\" class=\"k-button k-button-solid-error\" onclick=\"rndDP.fn_teamApp('N')\">마감취소</button>";
                         buttonHtml += '<div style="position: relative; top: 10px; right: 10px"><span style="float: right; color: red; font-size: 12px;">마감되었습니다.</span></div>';
 
                         /** 현재 버전이 완료 되었을때 버튼 비활성화 */
