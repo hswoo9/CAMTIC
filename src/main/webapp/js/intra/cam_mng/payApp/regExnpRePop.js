9a6842dae982e83c8ec3def02fef958ce90d5bf3
Hanker <anhan0804@dev-jitsu.com>
2024-04-23 20:27:26 +0900

    지출반제 확인

--- src/main/webapp/js/intra/cam_mng/payApp/regExnpRePop.js
+++ src/main/webapp/js/intra/cam_mng/payApp/regExnpRePop.js
@@ -312,6 +312,7 @@ var regExnpRe = {
             success : function(rs){
                 if(rs.code == 200){
                     alert("반제결의서 승인이 취소되었습니다.");
+                    location.reload()
                 }
             }
         });
@@ -568,7 +569,6 @@ var regExnpRe = {
             alert("사업을 선택해주세요.");
             return ;
         }
-
 
         var url = "/mng/pop/budgetView.do?pjtCd=" + $("#pjtCd").val() + "&idx=" + idx;
 
