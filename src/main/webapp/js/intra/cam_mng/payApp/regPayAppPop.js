e3c07fb091f33054c8f572b9cef8804402a6b8d0
jjhnn <hana8951@dev-jitsu.com>
2024-05-02 16:23:44 +0900

    지급신청 출장 법인차량 수정

--- src/main/webapp/js/intra/cam_mng/payApp/regPayAppPop.js
+++ src/main/webapp/js/intra/cam_mng/payApp/regPayAppPop.js
@@ -810,7 +810,7 @@ var regPay = {
                 $("#pjtNm").val(pjtMap.PJT_NM);
                 if ($("#pjtSn").val() != "" && busnClass != "D" && busnClass != "V") {
                     selectProject(pjtMap.PJT_SN, pjtMap.PJT_NM, pjtMap.PJT_CD);
-                    if (corpCar != null && corpCar.TOT_COST != null) {
+                    if (corpCar != null && (corpCar.TOT_COST != null || corpCar.TOT_COST != "0")) {
                         const g20CardList = customKendo.fn_customAjax("/g20/getCardList", {
                             searchValue: '법인차량',
                             cardVal: 'P'
