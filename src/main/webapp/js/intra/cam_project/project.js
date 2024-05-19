234bf4d91de162772888fbd77990d6c6126b1436
jsy <liveokde1004@dev-jitsu.com>
2024-05-19 13:34:57 +0900

    1. 캠프로젝트 : 프로젝트 팝업창 여러개 오픈하는 기능

--- src/main/webapp/js/intra/cam_project/project.js
+++ src/main/webapp/js/intra/cam_project/project.js
@@ -533,7 +533,7 @@ var camPrj = {
             } else if (cs == "S"){
                 url = "/projectUnRnd/pop/regProject.do?pjtSn=" + key;
             }
-            var name = "blank";
+            var name = "_blank";
             var option = "width = 1680, height = 850, top = 100, left = 200, location = no";
 
             var popup = window.open(url, name, option);
