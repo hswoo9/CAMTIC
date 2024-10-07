615c56fa7009628b8a812fbb39ca04cc22fa4fc7
jsy <liveokde1004@dev-jitsu.com>
2024-10-07 12:54:55 +0900

    지급신청서, 지급신청서 관리자 : 결재상태 분기에 20번(중간결재) 추가

--- src/main/webapp/js/intra/cam_mng/payApp/paymentMngList.js
+++ src/main/webapp/js/intra/cam_mng/payApp/paymentMngList.js
@@ -302,7 +302,7 @@ var paymentMngList = {
                             } else if (e.EXNP_STATUS != 0){
                                 stat = "지출대기";
                             }
-                        } else if(e.DOC_STATUS == "10" || e.DOC_STATUS == "50"){
+                        } else if(e.DOC_STATUS == "10" || e.DOC_STATUS == "20" || e.DOC_STATUS == "50"){
                             stat = "결재중"
                         } else if(e.DOC_STATUS == "30"){
                             stat = "반려"
